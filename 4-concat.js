fc65c87bde3c95f9b4c142364ee6824424873f64
Abubakari Issah <assaawin@outlook.com>
2025-11-06 12:13:56 +0000

    Add 4-concat.js - Print two arguments in 'is' format

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the first and second arguments
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print in the format: "firstArg is secondArg"
+console.log(firstArg + " is " + secondArg);
