f50cd8c0006b4221d0c1b0cf9590b8735da39da4
Abubakari Issah <assaawin@outlook.com>
2025-11-06 12:22:50 +0000

    Add 6-multi_languages_loop.js - Print languages using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Create an array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a loop to print each line with only one console.log
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
